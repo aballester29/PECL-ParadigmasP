0a158d46d9701d7350cb060e5b10b984f0ef7cc5
aballester29 <azahara.ballester@edu.uah.es>
2021-01-20 22:53:51 +0100

    PROGRAMA FINAL

--- src/RMI/Servidor.java
+++ src/RMI/Servidor.java
@@ -10,8 +10,8 @@ public class Servidor {
         try
         {
             Funcion obj = new Funcion(); //Crea una instancia del objeto que implementa la interfaz, como objeto a registrar 
-            Registry registry = LocateRegistry.createRegistry(1099); //Arranca rmiregistry local en el puerto 1099
-            Naming.rebind("//localhost/ObjetoRemoto",obj);   //rebind sólo funciona sobre una url del equipo local 
+            //Registry registry = LocateRegistry.createRegistry(1099); //Arranca rmiregistry local en el puerto 1099
+            Naming.rebind("//127.0.0.1/ObjetoRemoto",obj);   //rebind sólo funciona sobre una url del equipo local 
             System.out.println("El Objeto Remoto ha quedado registrado");
         }
         catch (Exception e)
